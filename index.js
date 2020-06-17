2fa98c1a5e9e6dfc2cb644e72b5c6dd8c394f597
Shreesh Nayak <shreeshnayak@gmail.com>
2020-06-17 00:34:13 -0700

    created server

--- index.js
+++ index.js
@@ -1,15 +1,90 @@
-/*
-const Person = require('./person')
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
 
-const person1 = new Person('John Doe', 30)
+const server = http.createServer((req, res) => {
+    /*
+    if(req.url === '/') {
+        fs.readFile(
+            path.join(__dirname, 'public', 'index.html'), 
+            (err, content) => {
+                if (err) throw err
+                res.writeHead(200, { 'Content-Type': 'text/html' })
+                res.end(content)                        
+            }
+        )
+    }
+    if(req.url === '/api/users') {
+        const users = [
+            { name: 'John Doe', age: 25 },
+            { name: 'Bob Smith', age: 30 }
+        ]
+        res.writeHead(200, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(users))                        
+    }
+    */
 
-person1.greeting()
-*/
+    // Build file path
+    let filePath = path.join(
+        __dirname, 
+        'public', 
+        req.url === '/' ? 'index.html' : req.url
+    )
+    
+    // Extension of file
+    let extname = path.extname(filePath)
+    
+    // Intial content type
+    let contentType = 'text/html'
 
-const Logger = require('./logger')
+    // Check extension and set content type
+    switch(extname) {
+        case '.js':
+            contentType = 'text/javascript'
+            break
+        case '.css':
+            contentType = 'text/css'
+            break
+        case '.json':
+            contentType = 'application/json'
+            break
+        case '.png':
+            contentType = 'image/png'
+            break
+        case '.jpg':
+            contentType = 'image/jpg'
+            break
+    }
 
-const logger = new Logger()
+    // Read file
+    fs.readFile(
+        filePath,
+        (err, content) => {
+            if(err) {
+                if(err.code == 'ENOENT') {
+                    // Page not found
+                    fs.readFile(
+                        path.join(__dirname, 'public', '404.html',
+                        (err, content) => {
+                            res.writeHead(200, {'Content-Type': 'text/html'})
+                            res.end(content, 'utf8')
+                        })
+                    )
+                } else {
+                    // Some server error
+                    res.writeHead(500)
+                    res.end(`Server Error: ${err.code}`)
+                }
+            } else {
+                // Success
+                res.writeHead(200, {'Content-Type': contentType})
+                res.end(content, 'utf8')
+            }
+        }
+    )
+    
+})
 
-logger.on('message', (data) => console.log('Called Listener', data))
+const PORT = process.env.PORT || 5000
 
-logger.log('Hello World')
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`))
--- logger.js
+++ logger.js
@@ -6,8 +6,20 @@ const uuid = require('uuid')
 class Logger extends EventEmitter {
     log(msg) {
         // Call event
-        this.emit('message', {id: uuid.v4(), msg})
+        this.emit('message', {id: uuid.v4(), msg}) // same as {id: uuid.v4(), msg: msg}
     }
 }
 
-module.exports = Logger
+//module.exports = Logger
+
+const Logger = require('./logger')
+
+const logger = new Logger()
+
+logger.on('message', (data) => console.log('Called Listener', data))
+
+logger.log('Hello World')
+
+
+// Homework Assignment
+// Bring in fs module and create a log file and write all the logged messages to that log file
